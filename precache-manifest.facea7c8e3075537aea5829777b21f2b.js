8dc1766212fc16a69e380b4a9af6c7df23011f63
prkkhan786 <prkkhan1@gmail.com>
2020-07-24 00:57:43 +0530

    Updates

---
+++ precache-manifest.facea7c8e3075537aea5829777b21f2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdabd206d7402a240a2a7ecbbf27164",
+    "url": "/NotesApp/index.html"
+  },
+  {
+    "revision": "717f86598dc465082811",
+    "url": "/NotesApp/static/css/2.566b319e.chunk.css"
+  },
+  {
+    "revision": "f9b49083a4badafdf742",
+    "url": "/NotesApp/static/css/main.9b629c6f.chunk.css"
+  },
+  {
+    "revision": "717f86598dc465082811",
+    "url": "/NotesApp/static/js/2.9c488ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NotesApp/static/js/2.9c488ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b49083a4badafdf742",
+    "url": "/NotesApp/static/js/main.bd9e4729.chunk.js"
+  },
+  {
+    "revision": "76b36b1cde110592502d",
+    "url": "/NotesApp/static/js/runtime-main.d7c01b6d.js"
+  }
+]);
